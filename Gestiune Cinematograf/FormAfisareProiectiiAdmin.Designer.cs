302ab0b32b23a68670b2fa1fd02d2c02f0819f62
davidsn7 <81225514+davidsn7@users.noreply.github.com>
2025-05-23 21:42:48 +0300

    Primul comit

--- Gestiune Cinematograf/FormAfisareProiectiiAdmin.Designer.cs
+++ Gestiune Cinematograf/FormAfisareProiectiiAdmin.Designer.cs
@@ -28,65 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dgvProiectii = new System.Windows.Forms.DataGridView();
+            this.cmbFilme = new System.Windows.Forms.ComboBox();
+            this.cmbSali = new System.Windows.Forms.ComboBox();
+            this.dtpOraProiectiei = new System.Windows.Forms.DateTimePicker();
+            this.cmbZi = new System.Windows.Forms.ComboBox();
+            this.txtPret = new System.Windows.Forms.TextBox();
             this.btnAdaugaProiectie = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProiectii)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvProiectii
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(34, 42);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvProiectii.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProiectii.Location = new System.Drawing.Point(34, 42);
+            this.dgvProiectii.Name = "dgvProiectii";
+            this.dgvProiectii.Size = new System.Drawing.Size(240, 150);
+            this.dgvProiectii.TabIndex = 0;
             // 
-            // comboBox1
+            // cmbFilme
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(34, 221);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
+            this.cmbFilme.FormattingEnabled = true;
+            this.cmbFilme.Location = new System.Drawing.Point(153, 218);
+            this.cmbFilme.Name = "cmbFilme";
+            this.cmbFilme.Size = new System.Drawing.Size(121, 21);
+            this.cmbFilme.TabIndex = 1;
             // 
-            // comboBox2
+            // cmbSali
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(34, 258);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 2;
+            this.cmbSali.FormattingEnabled = true;
+            this.cmbSali.Location = new System.Drawing.Point(153, 257);
+            this.cmbSali.Name = "cmbSali";
+            this.cmbSali.Size = new System.Drawing.Size(121, 21);
+            this.cmbSali.TabIndex = 2;
             // 
-            // dateTimePicker1
+            // dtpOraProiectiei
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(404, 24);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 3;
+            this.dtpOraProiectiei.Location = new System.Drawing.Point(404, 24);
+            this.dtpOraProiectiei.Name = "dtpOraProiectiei";
+            this.dtpOraProiectiei.Size = new System.Drawing.Size(200, 20);
+            this.dtpOraProiectiei.TabIndex = 3;
             // 
-            // comboBox3
+            // cmbZi
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(34, 296);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 21);
-            this.comboBox3.TabIndex = 4;
+            this.cmbZi.FormattingEnabled = true;
+            this.cmbZi.Location = new System.Drawing.Point(153, 295);
+            this.cmbZi.Name = "cmbZi";
+            this.cmbZi.Size = new System.Drawing.Size(121, 21);
+            this.cmbZi.TabIndex = 4;
             // 
-            // textBox1
+            // txtPret
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 335);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
+            this.txtPret.Location = new System.Drawing.Point(174, 337);
+            this.txtPret.Name = "txtPret";
+            this.txtPret.Size = new System.Drawing.Size(100, 20);
+            this.txtPret.TabIndex = 5;
             // 
             // btnAdaugaProiectie
             // 
-            this.btnAdaugaProiectie.Location = new System.Drawing.Point(34, 371);
+            this.btnAdaugaProiectie.Location = new System.Drawing.Point(199, 375);
             this.btnAdaugaProiectie.Name = "btnAdaugaProiectie";
             this.btnAdaugaProiectie.Size = new System.Drawing.Size(75, 23);
             this.btnAdaugaProiectie.TabIndex = 6;
@@ -94,21 +98,61 @@
             this.btnAdaugaProiectie.UseVisualStyleBackColor = true;
             this.btnAdaugaProiectie.Click += new System.EventHandler(this.btnAdaugaProiectie_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Filme";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Sali";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Zi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Pret";
+            // 
             // FormAfisareProiectiiAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAdaugaProiectie);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPret);
+            this.Controls.Add(this.cmbZi);
+            this.Controls.Add(this.dtpOraProiectiei);
+            this.Controls.Add(this.cmbSali);
+            this.Controls.Add(this.cmbFilme);
+            this.Controls.Add(this.dgvProiectii);
             this.Name = "FormAfisareProiectiiAdmin";
             this.Text = "FormAfisareProiectiiAdmin";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProiectii)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,12 +160,16 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dgvProiectii;
+        private System.Windows.Forms.ComboBox cmbFilme;
+        private System.Windows.Forms.ComboBox cmbSali;
+        private System.Windows.Forms.DateTimePicker dtpOraProiectiei;
+        private System.Windows.Forms.ComboBox cmbZi;
+        private System.Windows.Forms.TextBox txtPret;
         private System.Windows.Forms.Button btnAdaugaProiectie;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
